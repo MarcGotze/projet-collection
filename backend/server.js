28fdd00c99e7d8e316c33d3893c900d1ad551abb
Marc <marc.gotze@gmail.com>
2023-04-16 15:24:34 +0200

    Backend init

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,7 @@
 const express = require("express");
-const connectDB = require("./config/db");
-const dotenv = require("dotenv").config();
 const port = 5000;
 
-//Connexion à la DB
-connectDB();
-
 const app = express();
-
-//Middleware qui permet de traiter les données de la Request
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use("/post", require("./routes/post.routes"));
 
 //Lancement du serveur
 app.listen(port, () => console.log("Le serveur a démarré au port " + port));
