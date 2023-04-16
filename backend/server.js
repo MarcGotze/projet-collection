8c60a0e79253713f47d70c0adcab2e90edd23a19
Marc <marc.gotze@gmail.com>
2023-04-16 16:13:09 +0200

    Routes inti

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const port = 5000;
-
 const app = express();
+
+//Middleware traitant les données de la Request
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Appel des routes users
+app.use("/users", require("./routes/users.routes"));
 
 //Lancement du serveur
 app.listen(port, () => console.log("Le serveur a démarré au port " + port));
