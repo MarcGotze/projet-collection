564c3cf45b9ff2dea876feefcd5c49c54443e94e
Marc <marc.gotze@gmail.com>
2023-04-17 10:38:57 +0200

    Configuration de la connexion à MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv").config();
 const port = 5000;
 const app = express();
+
+//Connexion à la DB
+connectDB();
 
 //Middleware traitant les données de la Request
 app.use(express.json());
