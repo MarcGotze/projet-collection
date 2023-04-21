511931a1e3da40fe8ce38a07e5a55e53586cebef
Jean-Baptiste Maury <jeanbaptiste.maury@gmail.com>
2023-04-22 00:30:29 +0200

    Ajout Navigation et Login User

---
+++ src/pages/Profil.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Profil = () => {
+    return (
+        <div>
+
+            <h1>Profil utilisateur</h1>
+        </div>
+    );
+};
+
+export default Profil;
